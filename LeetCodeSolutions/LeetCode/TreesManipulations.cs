e9d88815f62424d761b1003ffac0e31ec9c5f778
Ester Kaufman <44020286+esterkaufman@users.noreply.github.com>
2022-01-31 21:25:09 +0200

    Symmetric Tree

---
+++ LeetCodeSolutions/LeetCode/TreesManipulations.cs
@@ -0,0 +1,25 @@
+﻿using LeetCodeSolutions.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.LeetCode
+{
+    internal class TreesManipulations
+    {
+        //Symmetric Tree
+        //https://leetcode.com/problems/symmetric-tree/discuss/1735021/simplest-and-70-time-faster-one-line-solution
+        public bool IsSymmetric(TreeNode root)
+        {
+            return root == null ? true : IsMirror(root, root);
+        }
+
+        public bool IsMirror(TreeNode left, TreeNode right)
+        {
+            return ((left == null && right == null) || (left?.val == right?.val &&
+                     IsMirror(left.left, right.right) && IsMirror(left.right, right.left)));
+        }
+    }
+}
